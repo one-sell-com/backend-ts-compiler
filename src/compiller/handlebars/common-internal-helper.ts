fe3e32cebf6878be644d6b3a54660edcf61a8658
Mohammad Karbalaei <mohammad.karbalaei@socialteq.com>
2020-12-31 16:01:43 +0330

    create stub and client interfaces just for common and internal proto files

--- src/compiller/handlebars/common-internal-helper.ts
+++ src/compiller/handlebars/common-internal-helper.ts
@@ -1,5 +1,8 @@
 import { registerHelper } from 'handlebars';
 
-registerHelper('common_internal', function (conditional) {
-    return conditional.indexOf('Common') > 0 || conditional.indexOf('Internal') > 0;
+registerHelper('ifCommonOrInternal', function (controllerName, options) {
+    if (controllerName.indexOf('Common') > 0 || controllerName.indexOf('Internal') > 0) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
 });
