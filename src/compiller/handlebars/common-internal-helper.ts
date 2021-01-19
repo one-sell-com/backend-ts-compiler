e661e91bd38137d1af8cac5b4016737055cfdacc
mortzprk <mortz.prk@gmail.com>
2021-01-20 02:14:33 +0330

    better types

--- src/compiller/handlebars/common-internal-helper.ts
+++ src/compiller/handlebars/common-internal-helper.ts
@@ -1,7 +1,7 @@
 import { registerHelper } from 'handlebars';
 
-registerHelper('ifCommonOrInternal', function (controllerName, options) {
-    if (controllerName.indexOf('Common') > 0 || controllerName.indexOf('Internal') > 0) {
+registerHelper('ifInternal', function (controllerName, options) {
+    if (controllerName.indexOf('Internal') > 0) {
         return options.fn(this);
     }
     return options.inverse(this);
